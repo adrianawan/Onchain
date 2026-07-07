88bf9953d4aceca6cc242c3cd5c7f33a47a77b73
adrianawan <adraianus.hermawan@gmail.com>
2026-07-07 10:52:33 +0700

    Add reset function

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,27 +2,35 @@
 pragma solidity ^0.8.28;
 
 contract Counter {
-    uint public x;
+    uint256 private count;
 
-    event Increment(uint by);
+    event Increment(uint256 by);
+    event Decrement(uint256 by);
+    event Reset();
 
     function inc() public {
-        x++;
+        count++;
         emit Increment(1);
     }
 
-    function incBy(uint by) public {
-        require(by > 0, "incBy: increment should be positive");
-        x += by;
+    function incBy(uint256 by) public {
+        require(by > 0, "Increment should be positive");
+        count += by;
         emit Increment(by);
     }
 
-    function getCount() public view returns (uint) {
-        return x;
+    function decrement() public {
+        require(count > 0, "Counter is already zero");
+        count--;
+        emit Decrement(1);
     }
 
-    function decrement() public {
-        require(x > 0, "Counter is already zero");
-        x--;
+    function reset() public {
+        count = 0;
+        emit Reset();
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
     }
 }
--- contracts/Counter.t.sol
+++ contracts/Counter.t.sol
@@ -5,25 +5,47 @@ import {Counter} from "./Counter.sol";
 import {Test} from "forge-std/Test.sol";
 
 contract CounterTest is Test {
-  Counter counter;
+    Counter counter;
 
-  function setUp() public {
-    counter = new Counter();
-  }
-
-  function test_InitialValue() public view {
-    require(counter.x() == 0, "Initial value should be 0");
-  }
-
-  function testFuzz_Inc(uint8 x) public {
-    for (uint8 i = 0; i < x; i++) {
-      counter.inc();
+    function setUp() public {
+        counter = new Counter();
     }
-    require(counter.x() == x, "Value after calling inc x times should be x");
-  }
 
-  function test_IncByZero() public {
-    vm.expectRevert();
-    counter.incBy(0);
-  }
+    function test_InitialValue() public view {
+        require(counter.getCount() == 0, "Initial value should be 0");
+    }
+
+    function testFuzz_Inc(uint8 x) public {
+        for (uint8 i = 0; i < x; i++) {
+            counter.inc();
+        }
+
+        require(
+            counter.getCount() == x,
+            "Value after calling inc x times should be x"
+        );
+    }
+
+    function test_IncByZero() public {
+        vm.expectRevert();
+        counter.incBy(0);
+    }
+
+    function test_Decrement() public {
+        counter.inc();
+        counter.decrement();
+
+        require(counter.getCount() == 0, "Decrement failed");
+    }
+
+
+    function test_Reset() public {
+        counter.inc();
+        counter.inc();
+
+        counter.reset();
+
+        require(counter.getCount() == 0, "Reset failed");
+    }
+
 }
